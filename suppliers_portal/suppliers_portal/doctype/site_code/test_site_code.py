517561f1b2ae655a665d0246282663d39de35b87
Rohan Bansal <rohanbansal33@gmail.com>
2025-06-17 11:44:28 +0530

    feat: add site code doctype

---
+++ suppliers_portal/suppliers_portal/doctype/site_code/test_site_code.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, jeowsome15@gmail.com and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSiteCode(FrappeTestCase):
+	pass
